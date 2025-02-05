9ac61cb289c403b10fe4d49634b609c52c2ee259
Sevriukoff <igor.sevriukoff@yandex.ru>
2025-02-05 12:19:40 +0700

    Add a basic game logic

--- SpaceShooter/Enemy.cs
+++ SpaceShooter/Enemy.cs
@@ -5,29 +5,13 @@ namespace SpaceShooter;
 
 public class Enemy : ScreenSurface, IMoveable
 {
-    private int _speed;
-    private int _tick;
-    
-    public Enemy(int speed) : base(1, 1)
+    public Enemy() : base(1, 1)
     {
-        _speed = speed;
-        
         Surface[0, 0].Glyph = 'V';
-
-        Surface[0, 0].Foreground = speed switch
-        {
-            1 => Color.Red,
-            2 or 3 => Color.Orange,
-            4 or 5 => Color.Yellow,
-            _ => Surface[0, 0].Foreground
-        };
     }
 
     public void Move()
     {
-        _tick++;
-        
-        if (_tick % _speed == 0)
-            Position += new Point(0, 1);
+        Position += new Point(0, 1);
     }
 }
--- SpaceShooter/GameScreen.cs
+++ SpaceShooter/GameScreen.cs
@@ -13,13 +13,10 @@ public class GameScreen : ScreenObject
         private Player _player;
         private List<Bullet> _bullets;
         private List<Enemy> _enemies;
-        private List<Supply> _supplies;
         private List<IMoveable> _moveableEntities;
         private Random _random;
-        private bool _isGameOver;
-        private int _score = 0;
-        private int _health = 1;
-        private int _ammo = 10;
+        private int _score;
+        private int _health = 3;
 
         public GameScreen()
         {
@@ -32,7 +29,6 @@ public class GameScreen : ScreenObject
 
             _bullets = new List<Bullet>();
             _enemies = new List<Enemy>();
-            _supplies = new List<Supply>();
             _random = new Random();
             
             Game.Instance.FrameUpdate += OnFrameUpdate;
@@ -40,12 +36,6 @@ public class GameScreen : ScreenObject
         
         public override bool ProcessKeyboard(Keyboard info)
         {
-            if (_isGameOver && info.KeysPressed.Count > 0)
-            {
-                RestartGame();
-                return true;
-            }
-            
             if (info.IsKeyPressed(Keys.Left) && _player.Position.X > 0)
                 _player.Position += new Point(-1, 0);
             else if (info.IsKeyPressed(Keys.Right) && _player.Position.X < 39)
@@ -55,21 +45,13 @@ public class GameScreen : ScreenObject
                 _player.Position += new Point(0, -1);
             else if (info.IsKeyPressed(Keys.Down) && _player.Position.Y < 25)
                 _player.Position += new Point(0, 1);
-            
+
             if (info.IsKeyPressed(Keys.Space))
             {
-                if (_ammo > 0)
-                {
-                    var bullet = new Bullet
-                    {
-                        Position = _player.Position + new Point(0, -1)
-                    };
-                    
-                    _bullets.Add(bullet);
-                    _console.Children.Add(bullet);
+                var bullet = new Bullet { Position = _player.Position + new Point(0, -1) };
 
-                    _ammo--;
-                }
+                _bullets.Add(bullet);
+                _console.Children.Add(bullet);
             }
 
             return base.ProcessKeyboard(info);
@@ -77,25 +59,12 @@ public class GameScreen : ScreenObject
 
         private void OnFrameUpdate(object? sender, GameHost e)
         {
-            if (_isGameOver)
-                return;
-            
             _console.Clear();
             _console.Print(1, 0, $"Score: {_score}");
-            _console.Print(13, 0, $"Ammo: {_ammo}");
             _console.Print(25, 0, $"Health: {_health}");
             
-            _moveableEntities = _bullets.Cast<IMoveable>().Concat(_enemies).Concat(_supplies).ToList();
+            _moveableEntities = _bullets.Cast<IMoveable>().Concat(_enemies).ToList();
             _moveableEntities.ForEach(x => x.Move());
-
-            foreach (var supply in _supplies.ToArray())
-            {
-                if (supply.IsExpired())
-                {
-                    _supplies.Remove(supply);
-                    _console.Children.Remove(supply);
-                }
-            }
             
             SpawnRandomEntities();
             
@@ -104,7 +73,6 @@ public class GameScreen : ScreenObject
 
             CheckPlayerEnemyCollisions();
             CheckBulletEnemyCollisions();
-            CheckPlayerSupplyCollisions();
         }
         
         private void CheckPlayerEnemyCollisions()
@@ -116,14 +84,6 @@ public class GameScreen : ScreenObject
                 RemoveEntity(enemy, _enemies);
                 
                 _health--;
-                
-                if (_health <= 0)
-                {
-                    _isGameOver = true;
-                    _console.Clear();
-                    _console.Print(15, 10, "GAME OVER", Color.Red, Color.Black);
-                    _console.Print(8, 12, "Press any key to restart", Color.Red, Color.Black);
-                }
                 
                 break;
             }
@@ -139,18 +99,6 @@ public class GameScreen : ScreenObject
                 RemoveEntity(bullet, _bullets);
                 RemoveEntity(enemy, _enemies);
                 _score += 10;
-                break;
-            }
-        }
-        
-        private void CheckPlayerSupplyCollisions()
-        {
-            foreach (var supply in _supplies.ToArray())
-            {
-                if (!_player.CheckCollision(supply)) continue;
-        
-                RemoveEntity(supply, _supplies);
-                _ammo += 5;
                 break;
             }
         }
@@ -172,45 +120,13 @@ public class GameScreen : ScreenObject
                     RemoveEntity(enemy, _enemies);
             }
         }
-        
-        private void RestartGame()
-        {
-            _isGameOver = false;
-            _health = 3;
-            _ammo = 10;
-            _score = 0;
-
-            _bullets.Clear();
-            _enemies.Clear();
-            _supplies.Clear();
-            _console.Children.Clear();
-
-            _player.Position = new Point(20, 22);
-            _console.Children.Add(_player);
-        }
 
 
         private void SpawnRandomEntities()
         {
-            TrySpawnEntity(1, () =>
-            {
-                var direction = _random.Next(0, 2);
-                
-                var position = direction == 0
-                    ? new Point(-5, _random.Next(6, 12))
-                    : new Point(45, _random.Next(6, 12));
-                
-                var ammo = new Supply(position.X < 0 ? 1 : -1)
-                {
-                    Position = position
-                };
-                
-                return (ammo, _supplies);
-            });
-            
             TrySpawnEntity(50, () =>
             {
-                var enemy = new Enemy(_random.Next(1, 5)){ Position = new Point(_random.Next(0, 40), 0) };
+                var enemy = new Enemy(){ Position = new Point(_random.Next(0, 40), 0) };
 
                 return (enemy, _enemies);
             });
